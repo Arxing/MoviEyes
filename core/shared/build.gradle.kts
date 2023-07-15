7b733b91ebff6379c0dadc3284f17092802bce28
Arxing <meiswind711108@gmail.com>
2023-07-15 22:38:51 +0800

    Define :core:shared

---
+++ core/shared/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+  id("me.android.library")
+  id("me.compose")
+  id("me.hilt")
+}
+
+android {
+  namespace = "org.me.core.shared"
+}
+
+dependencies {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,4 +18,5 @@ dependencyResolutionManagement {
 rootProject.name = "MoviEyes"
 include(
   ":app",
+  ":core:shared",
 )
