91a8c136116f67e01d430f373ef5ec310e8db8f5
Arxing <meiswind711108@gmail.com>
2023-07-15 22:56:41 +0800

    :core:shared add BaseComposeFragment

---
+++ core/shared/src/main/kotlin/org/me/core/shared/BaseComposeFragment.kt
@@ -0,0 +1,28 @@
+package org.me.core.shared
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+
+abstract class BaseComposeFragment : Fragment() {
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    return ComposeView(requireContext()).apply {
+      setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+      setContent {
+        MaterialTheme {
+          this@BaseComposeFragment.Content()
+        }
+      }
+    }
+  }
+
+  @Composable
+  abstract fun Content()
+}
